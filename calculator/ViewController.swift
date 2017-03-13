6f3fccd1795d4e73814acfacfeca1a50b22cb48a
liyi <s20151104707@mac24.local>
2017-03-13 11:34:16 +0800

    计算器1

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -3,13 +3,48 @@
 //  calculator
 //
 //  Created by s20151104707 on 17/3/13.
-//  Copyright © 2017年 s20151104707. All rights reserved.
+//  Copyright ©/Users/s20151104707/Desktop/calculator/calculator/Base.lproj/Main.storyboard 2017年 s20151104707. All rights reserved.
 //
 
 import UIKit
-
+var temp = ""
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func one(_ sender: Any) {
+        x.text = x.text! + "1"
+        
+    }
+    @IBAction func two(_ sender: Any) {
+        x.text = x.text! + "2"
+    }
+    @IBAction func three(_ sender: Any) {
+        x.text = x.text! + "3"
+    }
+
+    @IBAction func four(_ sender: Any) {
+        x.text = x.text! + "4"
+    }
+    @IBAction func five(_ sender: Any) {
+        x.text = x.text! + "5"
+    }
+    @IBAction func six(_ sender: Any) {
+        x.text = x.text! + "6"
+    }
+    @IBAction func seven(_ sender: Any) {
+        x.text = x.text! + "7"
+    }
+    @IBAction func eight(_ sender: Any) {
+        x.text = x.text! + "8"
+    }
+    @IBAction func nine(_ sender: Any) {
+        x.text = x.text! + "9"
+    }
+    @IBAction func zero(_ sender: Any) {
+        x.text = x.text! + "0"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
